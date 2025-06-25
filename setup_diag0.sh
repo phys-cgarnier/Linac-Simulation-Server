b4b0bb7687c77c6562883f79671c7a2dfd036924
chris garnier <cgarnier@slac.stanford.edu>
2025-06-25 10:37:45 -0700

    adding setup for diag0, should be separate from the run.sh script for now, i dont alwys want to start the server.

--- setup_diag0.sh
+++ setup_diag0.sh
@@ -3,8 +3,8 @@
 export EPICS_CA_ADDR_LIST=127.0.0.1
 export EPICS_CA_AUTO_ADDR_LIST="NO"
 export EPICS_CA_MAX_ARRAY_BYTES=1000000000
-#export EPICS_CA_SERVER_PORT=12345
+export EPICS_CA_SERVER_PORT=12345
 
 caRepeater&
 
-python simulated_server_diag0.py
+#python simulated_server_diag0.py
