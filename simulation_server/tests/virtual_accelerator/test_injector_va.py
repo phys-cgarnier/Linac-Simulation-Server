8377546b13ac148fbef900942b237bccb1646af6
Chris Garnier <cgarnier@slac.stanford.edu>
2025-12-18 15:17:25 -0800

    swapped labels on pvs back to previous commit

--- simulation_server/tests/virtual_accelerator/test_injector_va.py
+++ simulation_server/tests/virtual_accelerator/test_injector_va.py
@@ -18,8 +18,8 @@ class TestInjectorVA:
             "OTRS:IN20:571:Image:ArraySize0_RBV",
         ])
         image = output[otr_pv]
-        size_x = output["OTRS:IN20:571:Image:ArraySize0_RBV"]
-        size_y = output["OTRS:IN20:571:Image:ArraySize1_RBV"]
+        size_y = output["OTRS:IN20:571:Image:ArraySize0_RBV"]
+        size_x = output["OTRS:IN20:571:Image:ArraySize1_RBV"]
         image = np.array(image).reshape((size_x, size_y))
 
         plt.figure()
